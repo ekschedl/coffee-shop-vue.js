638fa59715afc04d4e2c8035058b1033c7b83abb
Ekaterina Schedlberger <ek.schedl@gmail.com>
2025-01-20 12:24:50 +0100

    + homework lesson06 links in FooterComponent

--- coffee-shop/src/components/FooterComponent.vue
+++ coffee-shop/src/components/FooterComponent.vue
@@ -4,32 +4,24 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <ul class="footer d-flex flex-wrap">
-            <li class="footer__item">
-              <router-link :to="links[0].link">
+              <list-item
+                  :link="links.header.link"
+                  :classList="'footer__item'"
+                > 
                 <img
-                  :src="require(`@/assets/logo/${links[0].icon}`)"
-                  :alt="links[0].icon"
+                  :src="require(`@/assets/logo/${links.header.icon}`)"
+                  :alt="links.header.icon"
                 />
-              </router-link>
-            </li>
+              </list-item>
 
-            <list-item
-              :link="links[1].link"
-              :text="links[1].text"
-              :classList="'footer__item'"
-            ></list-item>
-
-            <list-item
-              :link="links[2].link"
-              :text="links[2].text"
-              :classList="'footer__item'"
-            ></list-item>
-
-            <list-item
-              :link="links[3].link"
-              :text="links[3].text"
-              :classList="'footer__item'"
-            ></list-item>
+              <list-item
+                v-for="link in links.other"
+                :key="link.id"
+                :link="link.link"
+                :text="link.text"
+                :classList="'footer__item'"
+              ></list-item>
+           
           </ul>
         </div>
       </div>
@@ -49,28 +41,31 @@ export default {
   components: { ListItem },
   data() {
     return {
-      links: [
-        {
-          id: 0,
-          icon: "Logo_black.svg",
-          link: "/",
-        },
-        {
-          id: 1,
-          text: "Our Coffee",
-          link: "/our-coffee",
-        },
-        {
-          id: 2,
-          text: "For your pleasure",
-          link: "/goods",
-        },
-        {
-          id: 3,
-          text: "Contact us",
-          link: "/contact",
-        },
-      ],
+      links: {
+        header:  {
+            id: 0,
+            icon: "Logo_black.svg",
+            link: "/",
+          },
+        other:
+          [
+            {
+              id: 1,
+              text: "Our Coffee",
+              link: "/our-coffee",
+            },
+            {
+              id: 2,
+              text: "For your pleasure",
+              link: "/goods",
+            },
+            {
+              id: 3,
+              text: "Contact us",
+              link: "/contact",
+            },
+          ],
+      },
     };
   },
 };
