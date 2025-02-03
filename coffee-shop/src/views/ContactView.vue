f33acbed70a3783adc2f046b29de76c2ffd920a2
Ekaterina Schedlberger <ek.schedl@gmail.com>
2025-02-03 21:14:46 +0100

    + lesson10 useVuelidate

--- coffee-shop/src/views/ContactView.vue
+++ coffee-shop/src/views/ContactView.vue
@@ -23,40 +23,64 @@
 
             <form @submit.prevent="submit" action="/" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input v-model="form.name" type="text" class="form-control" id="name-input" />
+                  <input
+                    v-model="v$.name.$model"
+                    type="text"
+                    class="form-control"
+                    id="name-input"
+                  />
+                  <span v-for="error in v$.name.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
                   </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input v-model="form.email"type="email" class="form-control" id="email-input" />
+                  <input
+                    v-model="v$.email.$model"
+                    type="email"
+                    class="form-control"
+                    id="email-input"
+                  />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
-                  <input v-model="form.phone"type="tel" class="form-control" id="phone-input" />
+                  <input
+                    v-model="v$.phone.$model"
+                    type="tel"
+                    class="form-control"
+                    id="phone-input"
+                  />
+                  <span v-for="error in v$.phone.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -69,14 +93,19 @@
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
-                    v-model="form.message"
+                    v-model="v$.message.$model"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">
+                    {{ error.$message }}
+                  </span>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col">
-                  <button  type="submit" class="btn btn-outline-dark send-btn">Send us</button>
+                  <button type="submit" class="btn btn-outline-dark send-btn">
+                    Send us
+                  </button>
                 </div>
               </div>
             </form>
@@ -90,24 +119,36 @@
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
 import TitelHeader from "@/components/TitelHeader.vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, maxLength } from "@vuelidate/validators";
 
 export default {
-  components: { NavBarComponent, TitelHeader },
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
       pageTitle: "Contact us",
-      form: {
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
-      },
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    };
+  },
+  components: {
+    NavBarComponent,
+    TitelHeader,
+  },
+  validations() {
+    return {
+      name: { required },
+      email: { required, email },
+      phone: {},
+      message: { required, maxLength: maxLength(20) },
     };
   },
   methods: {
-    submit() {
-      console.log(this.form);
-    },
+    submit() {},
   },
 };
 </script>
